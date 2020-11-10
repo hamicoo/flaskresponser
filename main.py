585113651cda9d279fa2cec2820a740c275c77d0
Hamed Azizi <you@example.com>
2020-11-10 11:43:05 +0100

    first v

---
+++ main.py
@@ -0,0 +1,31 @@
+
+
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+
+
+def calculator(num):
+    if str(num).isnumeric():
+        return (True,int(num)*2)
+    else:
+        return (False,"please give me a number")
+    
+
+
+@app.route('/calculator/<number>')
+def hello(number):
+    res=calculator (number)
+    if res[0]:
+        return jsonify({ "message":"ok" , "calculation_result" : res[1] })
+    return jsonify({ "message":"Err" , "calculation_result" : str(res[1]) })
+    
+        
+    
+    
+    
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
